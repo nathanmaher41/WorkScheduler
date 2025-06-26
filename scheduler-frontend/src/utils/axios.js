d40f3bdeb0b0a61739e35982ac93c4720a7054cf
Nathan Maher <nathantmaher@gmail.com>
2025-06-25 20:56:37 -0400

    frontend stuff for netlify

--- scheduler-frontend/src/utils/axios.js
+++ scheduler-frontend/src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const instance = axios.create({
+//   baseURL: 'http://localhost:8000',
+// });
+
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Attach access token to every request
--- scheduler-frontend/src/utils/axiosPublic.js
+++ scheduler-frontend/src/utils/axiosPublic.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// const axiosPublic = axios.create({
+//   baseURL: 'http://localhost:8000',
+// });
+
+
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosPublic;
